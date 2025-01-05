65761de9c1de1d88e87e411578166af0244d647c
“li-peifeng” <peifeng@me.com>
2025-01-05 02:04:23 +0100

    isweet

--- server/handles/sshkey.go
+++ server/handles/sshkey.go
@@ -1,11 +1,12 @@
 package handles
 
 import (
+	"strconv"
+
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/op"
 	"github.com/alist-org/alist/v3/server/common"
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
 
 type SSHKeyAddReq struct {
@@ -16,16 +17,16 @@ type SSHKeyAddReq struct {
 func AddMyPublicKey(c *gin.Context) {
 	userObj, ok := c.Value("user").(*model.User)
 	if !ok || userObj.IsGuest() {
-		common.ErrorStrResp(c, "user invalid", 401)
+		common.ErrorStrResp(c, "当前用户无效", 401)
 		return
 	}
 	var req SSHKeyAddReq
 	if err := c.ShouldBind(&req); err != nil {
-		common.ErrorStrResp(c, "request invalid", 400)
+		common.ErrorStrResp(c, "请求无效", 400)
 		return
 	}
 	if req.Title == "" {
-		common.ErrorStrResp(c, "request invalid", 400)
+		common.ErrorStrResp(c, "请求无效", 400)
 		return
 	}
 	key := &model.SSHPublicKey{
@@ -35,7 +36,7 @@ func AddMyPublicKey(c *gin.Context) {
 	}
 	err, parsed := op.CreateSSHPublicKey(key)
 	if !parsed {
-		common.ErrorStrResp(c, "provided key invalid", 400)
+		common.ErrorStrResp(c, "提供的密钥无效", 400)
 		return
 	} else if err != nil {
 		common.ErrorResp(c, err, 500, true)
@@ -47,7 +48,7 @@ func AddMyPublicKey(c *gin.Context) {
 func ListMyPublicKey(c *gin.Context) {
 	userObj, ok := c.Value("user").(*model.User)
 	if !ok || userObj.IsGuest() {
-		common.ErrorStrResp(c, "user invalid", 401)
+		common.ErrorStrResp(c, "当前用户无效", 401)
 		return
 	}
 	list(c, userObj)
@@ -56,17 +57,17 @@ func ListMyPublicKey(c *gin.Context) {
 func DeleteMyPublicKey(c *gin.Context) {
 	userObj, ok := c.Value("user").(*model.User)
 	if !ok || userObj.IsGuest() {
-		common.ErrorStrResp(c, "user invalid", 401)
+		common.ErrorStrResp(c, "当前用户无效", 401)
 		return
 	}
 	keyId, err := strconv.Atoi(c.Query("id"))
 	if err != nil {
-		common.ErrorStrResp(c, "id format invalid", 400)
+		common.ErrorStrResp(c, "ID格式无效", 400)
 		return
 	}
 	key, err := op.GetSSHPublicKeyByIdAndUserId(uint(keyId), userObj.ID)
 	if err != nil {
-		common.ErrorStrResp(c, "failed to get public key", 404)
+		common.ErrorStrResp(c, "获取公钥失败", 404)
 		return
 	}
 	err = op.DeleteSSHPublicKeyById(key.ID)
@@ -80,12 +81,12 @@ func DeleteMyPublicKey(c *gin.Context) {
 func ListPublicKeys(c *gin.Context) {
 	userId, err := strconv.Atoi(c.Query("uid"))
 	if err != nil {
-		common.ErrorStrResp(c, "user id format invalid", 400)
+		common.ErrorStrResp(c, "用户ID格式无效", 400)
 		return
 	}
 	userObj, err := op.GetUserById(uint(userId))
 	if err != nil {
-		common.ErrorStrResp(c, "user invalid", 404)
+		common.ErrorStrResp(c, "当前用户无效", 404)
 		return
 	}
 	list(c, userObj)
@@ -94,7 +95,7 @@ func ListPublicKeys(c *gin.Context) {
 func DeletePublicKey(c *gin.Context) {
 	keyId, err := strconv.Atoi(c.Query("id"))
 	if err != nil {
-		common.ErrorStrResp(c, "id format invalid", 400)
+		common.ErrorStrResp(c, "ID格式无效", 400)
 		return
 	}
 	err = op.DeleteSSHPublicKeyById(uint(keyId))
